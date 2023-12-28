d7b8ee57cbcfd7758416efc7ee83e9926de2f1f7
Rodrigo Augusto <rodrigoaugustok03@gmail.com>
2023-12-28 15:29:01 -0300

    Melhorando o codigo

--- modulo_4/routes/perfil.js
+++ modulo_4/routes/perfil.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (_, res) => {
-    res.render('perfil/index');
+    res.render('perfil/index', {
+        nome: 'Rodrigo Augusto Gonçalves Dalosto',
+        idade: 21,
+    });
 });
 
 module.exports = router;
